4b29e3b29dc4ab6627855134df789df6fad004dc
Qhan <qhan555@163.com>
2018-11-09 08:15:12 -0500

    minor improvent

--- PSO_class.py
+++ PSO_class.py
@@ -14,8 +14,8 @@ class PSO:
 	    self.c1=2.0
 	    self.c2=2.0
 	    self.pN=pN
-	    self.lb=-12.
-	    self.ub=12.
+	    self.lb=-1.
+	    self.ub=1.
 	    self.dim=dim
 	    self.max_iter=max_iter
 	    self.X=zeros((self.pN,self.dim))     ## particle's positions and velocity 
@@ -36,9 +36,9 @@ class PSO:
         #-----------target function---------#
 	def function(self,X): ##X is local
 #	    return X*sin(X)+X*cos(2*X)  
-#	    return cos(14.5 * X - 0.3) + (X + 0.2) * X
+	    return cos(14.5 * X - 0.3) + (X + 0.2) * X
 #	    return X**2-4*X+3  
- 	    return (X**2-5*X)*sin(X) 
+# 	    return (X**2-5*X)*sin(X) 
 #	    return LA.norm(X)**2
 #	    return (X[0]**2+X[1]-11)**2+(X[0]+X[1]**2-7)**2
 
@@ -54,8 +54,6 @@ class PSO:
 		if tmp<self.g_fit:
 		   self.g_fit=tmp
 		   self.gbest=self.X[i]
-#	    print 'g_fit=',self.g_fit,self.gbest
-	    
 
         #-------------PSO iteration-------------#
 	def iterator(self):
@@ -67,8 +65,6 @@ class PSO:
 		 gbestls.append(self.gbest)
 		 for j in range(self.pN):
 		     tmp=self.function(self.X[j])
-#		     if j==0:
-#		         print i,j,'x=',self.X[j],'y=',tmp
 		     if tmp<self.p_fit[j]:  ##updating personal best
 		        self.pbest[j]=self.X[j]
 			self.p_fit[j]=tmp
@@ -78,8 +74,6 @@ class PSO:
 		     self.V[j]=self.w*self.V[j]+self.c1*random.rand(self.dim)*(self.pbest[j]-self.X[j])+self.c2*random.rand(self.dim)*(self.gbest-self.X[j])
 		     ##velocity normalization##
 		     vnom=LA.norm(self.V[j])
-#		     if j==0:
-#		       print 'vnorm',vnom
 		     if vnom>1.0:
 		     	 self.V[j]/=vnom
 		     self.X[j]+=self.V[j]
@@ -94,28 +88,27 @@ class PSO:
 
 
 if __name__=="__main__":
-	my_pso=PSO(pN=20,dim=1,max_iter=100)
+	my_pso=PSO(pN=20,dim=1,max_iter=200)
 	my_pso.print_info()
 	my_pso.init_Population()
 	fitness,gbestls=my_pso.iterator()
 	
 	print "global minimum x=",gbestls[-1]
 	print "global minimum y=",fitness[-1]
- 	#print fitness
 
 	figure(1)
 	title('Global_best y vs iteration')
-	plot(range(my_pso.max_iter),fitness,'o-')
+	plot(range(my_pso.max_iter),fitness,'o-',markersize=2)
 	for i in range(my_pso.dim):
 	    figure(i+2)
 	    title('Global_best x_'+str(i)+' vs iteration')
-	    plot(range(my_pso.max_iter),gbestls[:,i],'o-')
+	    plot(range(my_pso.max_iter),gbestls[:,i],'o-',markersize=2)
 		
 	figure(0)
-	xs=linspace(my_pso.lb,my_pso.ub,200)
-	ys=[my_pso.function(x) for x in xs]
-	plot(xs,ys)
 	if my_pso.dim==1:
+	   xs=linspace(my_pso.lb,my_pso.ub,200)
+ 	   ys=[my_pso.function(x) for x in xs]
+	   plot(xs,ys,'o-',markersize=2)
     	   axvline(x=gbestls[-1],c='r')
 	   savefig('PSO_globalmin.png')
 	show()
